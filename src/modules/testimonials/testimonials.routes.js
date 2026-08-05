90d01de30e4ac504aa329fc599e58afb5023baaa
sujalraval <88503112+sujalraval@users.noreply.github.com>
2026-08-06 02:20:32 +0530

    fix: auth mapping

--- src/modules/testimonials/testimonials.routes.js
+++ src/modules/testimonials/testimonials.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('./testimonials.controller');
-const { protect } = require('../../middlewares/auth.middleware');
+const { authenticateToken: protect } = require('../../middlewares/auth.middleware');
 
 router.get('/', controller.getAll);
 router.get('/:id', controller.getById);
