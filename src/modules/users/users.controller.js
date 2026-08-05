4ff04a637b310ea590d300dfd70b1ab13770d953
sujalraval <88503112+sujalraval@users.noreply.github.com>
2026-08-06 02:49:30 +0530

    fix: use bcrypt instead of bcryptjs in users controller

--- src/modules/users/users.controller.js
+++ src/modules/users/users.controller.js
@@ -1,6 +1,6 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 
 exports.getAll = async (req, res) => {
   try {
